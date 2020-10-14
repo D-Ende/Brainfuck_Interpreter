239e8eca98665bd0963fcb330c7804650995a069
D-Ende <DeniseEnde@home24.de>
2020-10-14 09:27:26 +0200

    Code function better

--- bfinterpreter.go
+++ bfinterpreter.go
@@ -11,15 +11,14 @@ var scanner = bufio.NewScanner(os.Stdin)
 
 // Menu, choices to add Brainfuckcode
 func code() []byte {
+	returns := make([]byte, 1000000)
 
 	fmt.Println("Do You want to write your own Brainfuck code or read a file? ")
 	fmt.Println("write Text or File")
 	scanner.Scan()
 
-	returns := make([]byte, 1000000)
-
 	if scanner.Text() == "Text" {
-		fmt.Println("Enter your Brainfuck code and let the magig begin (:")
+		fmt.Println("Enter your Brainfuck code and let the magic begin (:")
 		scanner.Scan()
 		returns = scanner.Bytes()
 
@@ -51,7 +50,6 @@ func main() {
 		switch bfCode[codePointer] {
 
 		case 60: // Instruction "<"
-
 			if prgPointer != 29999 {
 				prgPointer--
 			} else if prgPointer == 29999 {
@@ -59,7 +57,6 @@ func main() {
 			}
 
 		case 62: // Instruction ">"
-
 			if prgPointer != 29999 {
 				prgPointer++
 			} else if prgPointer == 0 {
@@ -67,7 +64,6 @@ func main() {
 			}
 
 		case 43: // Instruction "+"
-
 			if programm[prgPointer] != 255 {
 				programm[prgPointer]++
 			} else if programm[prgPointer] == 255 {
@@ -75,7 +71,6 @@ func main() {
 			}
 
 		case 45: // Instruction "-"
-
 			if programm[prgPointer] != 0 {
 				programm[prgPointer]--
 			} else if programm[prgPointer] == 0 {
@@ -83,7 +78,6 @@ func main() {
 			}
 
 		case 91: // Instruction "["
-
 			if programm[prgPointer] == 0 {
 
 				loopCount := 0
@@ -104,7 +98,6 @@ func main() {
 			}
 
 		case 93: // Instruction "]"
-
 			if programm[prgPointer] != 0 {
 
 				loopCount := 0
@@ -112,7 +105,7 @@ func main() {
 
 				for codePointer < len(bfCode) {
 					if bfCode[codePointer] == 91 && loopCount == 0 {
-						prgPointer--
+						codePointer--
 						break
 					} else if bfCode[codePointer] == 91 {
 						loopCount--
@@ -125,11 +118,9 @@ func main() {
 			}
 
 		case 46: // Instruction "."
-
 			fmt.Print(string(programm[prgPointer]))
 
 		case 44: // Instruction ","
-
 			fmt.Println("Please enter one character..")
 			scanner.Scan()
 			programm[prgPointer] = scanner.Bytes()[0]
